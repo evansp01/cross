cf5790f80257e2f79ba2d70891ba1a3eb7018028
Evan Palmer <evansp01@gmail.com>
2021-04-21 20:41:59 -0400

    black magic to factor out sidebar

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgbNav } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
+  @ViewChild('nav') nav!: NgbNav;
 
   constructor() { }
 
